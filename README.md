3d943ba0aa6b30a57f2e631fde943999019fed75
Simon Beck <simon.beck@vshn.ch>
2018-09-27 16:47:57 +0200

    Restore to disk and S3
    This commit implements restores to local disk and to a s3 bucket. Please
    see the readme on how to use them.
    
    Relates to: AMZE-996

--- README.md
+++ README.md
@@ -36,4 +36,19 @@ Run a check of the repository:
 docker run -e "HOSTNAME=test" -e "PROM_URL=http://192.168.1.43:9091" -v /path/to/back:/data/ wrestic/wrestic -check
 ```
 
+Run a restore to disk:
+```
+docker run -e "HOSTNAME=test" -v /path/for/restore:/restore wrestic -restore -restoreType folder
+```
+
+Run a restore to disk with a filter:
+```
+docker run -e "HOSTNAME=test" -v /patch/for/restore:/restore wrestic -restore -restoreType folder -restoreFilter /var/mysql
+```
+
+Run a restore to S3:
+```
+docker run -e "HOSTNAME=test" -e "RESTORE_S3ENDPOINT=http://localhost:9000/bucketName" -e "RESTORE_ACCESSKEYID=1324" -e "RESTORE_SECRETACCESSKEY=secret" wrestic -restore -restoreType s3
+```
+
 The container will exit after the job is done. If a valid `PROM_URL` is provided it will push metrics there.
---
+++ s3/client.go
@@ -0,0 +1,75 @@
+package s3
+
+import (
+	"fmt"
+	"io"
+	"net/url"
+	"strings"
+
+	"github.com/minio/minio-go"
+)
+
+// Client wraps the minio s3 client
+type Client struct {
+	Endpoint        string
+	AccessKeyID     string
+	SecretAccessKey string
+	minioClient     *minio.Client
+	bucket          string
+}
+
+type UploadObject struct {
+	ObjectStream io.Reader
+	Name         string
+}
+
+// New returns a new Client
+func New(endpoint, accessKeyID, secretAccessKey string) *Client {
+	return &Client{
+		Endpoint:        endpoint,
+		AccessKeyID:     accessKeyID,
+		SecretAccessKey: secretAccessKey,
+	}
+}
+
+// Connect creates a minio client
+func (c *Client) Connect() error {
+
+	u, err := url.Parse(c.Endpoint)
+
+	var ssl bool
+	if u.Scheme == "https" {
+		ssl = true
+	} else if u.Scheme == "http" {
+		ssl = false
+	} else {
+		return fmt.Errorf("Endpoint %v has wrong scheme (http/https)", c.Endpoint)
+	}
+
+	c.bucket = strings.Replace(u.Path, "/", "", 1)
+	c.Endpoint = u.Host
+	mc, err := minio.New(c.Endpoint, c.AccessKeyID, c.SecretAccessKey, ssl)
+	c.minioClient = mc
+
+	if err == nil {
+		err = c.createBucket()
+	}
+
+	return err
+}
+
+func (c *Client) createBucket() error {
+	exists, err := c.minioClient.BucketExists(c.bucket)
+	if !exists && err == nil {
+		return c.minioClient.MakeBucket(c.bucket, "")
+	} else if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Upload uploads a io.Reader object to the configured endpoint
+func (c *Client) Upload(object UploadObject) error {
+	_, err := c.minioClient.PutObject(c.bucket, object.Name, object.ObjectStream, -1, minio.PutObjectOptions{})
+	return err
+}
