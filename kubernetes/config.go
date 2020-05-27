0350cf8ea038d7c41678bb0830399a5015e65080
Simon Beck <simon.beck@vshn.ch>
2020-05-27 12:46:00 +0200

    Avoid errors during integration tests
    This commit will check if it's prossible to connect to k8s before
    attempting so. This will reduce the amount of false positive logs during
    integration testing.

--- kubernetes/config.go
+++ kubernetes/config.go
@@ -10,12 +10,15 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+var (
+	Kubeconfig = filepath.Join(os.Getenv("HOME"), ".kube", "config")
+)
+
 func getClientConfig() (*rest.Config, error) {
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		err1 := err
-		kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		config, err = clientcmd.BuildConfigFromFlags("", Kubeconfig)
 		if err != nil {
 			err = fmt.Errorf("InClusterConfig as well as BuildConfigFromFlags Failed. Error in InClusterConfig: %+v\nError in BuildConfigFromFlags: %+v", err1, err)
 			return nil, err
